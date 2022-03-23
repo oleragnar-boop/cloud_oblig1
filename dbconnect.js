85d56d7f3fd8c467684b30a4da0b045583102ef5
daniepbj <72884521+daniepbj@users.noreply.github.com>
2022-03-23 20:53:53 +0100

    remove env

--- dbconnect.js
+++ dbconnect.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+    try {
+        const conn = await mongoose.connect('mongodb+srv://admin:adminpassword@cluster0.qmp2g.mongodb.net/user_data?retryWrites=true&w=majority')
         console.log('Connected to db using env')
-    }catch (error){
+    } catch (error) {
         console.log(error)
     }
 }
