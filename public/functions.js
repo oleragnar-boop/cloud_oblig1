acda63bd0d16217e20aa401fb1481682a02cf310
oleragnar-boop <72004318+oleragnar-boop@users.noreply.github.com>
2022-03-24 17:44:35 +0100

    Add files via upload

---
+++ public/functions.js
@@ -0,0 +1,15 @@
+
+const deleteButton = document.querySelector('#deleteButton');
+const searchButton = document.querySelector('#searchButton');
+
+
+searchButton.addEventListener('click', _ => {
+    let currentdId = document.querySelector('#studId').value;
+    console.log(req.query)
+  });
+
+
+deleteButton.addEventListener('click', _ => {
+let currentdId = document.querySelector('#studId').value;
+console.log(req.query)
+});
