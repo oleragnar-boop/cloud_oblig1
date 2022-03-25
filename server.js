a84dd6b357b8a312ca687b5540dfd5798cf3c44e
oleragnar-boop <72004318+oleragnar-boop@users.noreply.github.com>
2022-03-25 11:53:43 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,104 +1,118 @@
+//Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 const Users = require('./userSchema')
 
 
+//This project was made by Ole Ragnar Randen and Daniel Peder Björk as a part of the subject IDG2001 Cloud Technologies 
 
-    MongoClient.connect('mongodb+srv://admin:adminpassword@cluster0.qmp2g.mongodb.net/user_data?retryWrites=true&w=majority', { useUnifiedTopology: true })
+
+//Connecting to the database via MongoCLient
+MongoClient.connect('mongodb+srv://admin:adminpassword@cluster0.qmp2g.mongodb.net/user_data?retryWrites=true&w=majority', { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('user_data')
     const dataCollection = db.collection('user_data')
 
+    //Making the view of this app ejs
     app.set('view engine', 'ejs')
 
     app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('listening on 3000')
-  })
+    app.listen(process.env.PORT || 3000, function () {
+      console.log('listening on 3000')
+    })
 
-  app.get('/', (req, res) => {
-        res.render('index.ejs')
+    //GET to serve the index.ejs page
+    app.get('/', (req, res) => {
+      res.render('index.ejs')
+    })
+
+    //GET to fetch all the students from the user_data collection in mongodb
+    app.get('/data', async (req, res) => {
+      db.collection('user_data').find().toArray()
+        .then(results => {
+          res.render('data', { user_data: results, currentId: req.query.searchid, currentdegree: req.query.degree })
+        })
+    })
+
+    //GET to fetch one student from the user_data collection in mongodb that matches the currentid
+    //uses req.query.searchid to find the id currently being searched
+    app.get('/getstudent', async (req, res) => {
+      let currentid = parseInt(req.query.searchid);
+      let currentdegree = req.query.degree;
+      db.collection('user_data').find({ student_id: currentid }).toArray()
+        .then(results => {
+          res.render('data', { user_data: results, currentId: currentid, currentdegree: currentdegree })
+        })
+    })
+
+    //GET to fetch all students from the user_data collection in mongodb that matches the degree currently being searched
+    //uses req.query.degree to find the degree currently being searched
+    app.get('/getdegrees', async (req, res) => {
+      let currentid = parseInt(req.query.searchid);
+      let currentdegree = req.query.degree;
+      db.collection('user_data').find({ degree: currentdegree }).toArray()
+        .then(results => {
+          res.render('data', { user_data: results, currentdegree: currentdegree, currentId: currentid })
+        })
+    })
+
+    //GET that functions as a DELETE, uses the current searchid to find and delete a user with a matching id from the database 
+    app.get('/delstudent', async (req, res) => {
+      let currentid = parseInt(req.query.searchid);
+      let currentdegree = req.query.degree;
+      db.collection('user_data').findOneAndDelete({ student_id: currentid })
+        .then(results => {
+          res.render('data', { user_data: results, currentId: currentid, currentdegree: currentdegree })
+          console.log("Student", currentid, "Deleted")
+        })
+    })
+
+    //POST that adds users from the Add user form to the database based on the userSchema
+    app.post('/', async (req, res) => {
+      let newUser = new Users({
+        name: req.body.name,
+        surname: req.body.surname,
+        student_id: req.body.student_id,
+        age: req.body.age,
+        nationality: req.body.nationality,
+        degree: req.body.degree,
+        dateAdded: req.body.dateAdded
       })
-
-  app.get('/data', async (req, res) => {
-    db.collection ('user_data').find().toArray()
-    .then(results => {
-      res.render('data', {user_data: results, currentId: req.query.searchid, currentdegree: req.query.degree})
+      try {
+        await mongoose.connect('mongodb+srv://admin:adminpassword@cluster0.qmp2g.mongodb.net/user_data?retryWrites=true&w=majority', { useUnifiedTopology: true })
+        newUser.save()
+        res.redirect('/')
+        console.log(newUser)
+      } catch (err) {
+        console.log(err)
+      }
     })
-})
 
-app.get('/getstudent', async (req, res) => {
-  let currentid = parseInt(req.query.searchid);
-  let currentdegree = req.query.degree;
-      db.collection ('user_data').find({student_id: currentid}).toArray()
-      .then(results => {
-        res.render('data', {user_data: results, currentId: currentid,  currentdegree: currentdegree})
-    })
-})
-
-app.get('/getdegrees', async (req, res) => {
-  let currentid = parseInt(req.query.searchid);
-  let currentdegree = req.query.degree;
-      db.collection ('user_data').find({degree: currentdegree}).toArray()
-      .then(results => {
-        res.render('data', {user_data: results, currentdegree: currentdegree, currentId: currentid})
-    })
-})
-
-app.get('/delstudent', async (req, res) => {
-  let currentid = parseInt(req.query.searchid);
-  let currentdegree = req.query.degree;
-      db.collection ('user_data').findOneAndDelete({student_id: currentid})
-      .then(results => {
-        res.render('data', {user_data: results, currentId: currentid,  currentdegree: currentdegree})
-        console.log("Student", currentid, "Deleted")
-    })
-})
-
-  app.post('/', async (req, res) => {
-    let newUser = new Users({
-      name: req.body.name,
-      surname: req.body.surname,
-      student_id: req.body.student_id,
-      age: req.body.age,
-      nationality: req.body.nationality,
-      degree: req.body.degree,
-      dateAdded: req.body.dateAdded
-    })
-    try{
-      await mongoose.connect('mongodb+srv://admin:adminpassword@cluster0.qmp2g.mongodb.net/user_data?retryWrites=true&w=majority', { useUnifiedTopology: true })
-      newUser.save()
-      res.redirect('/')
-      console.log(newUser)
-    }catch (err){
-    console.log(err)
-  } 
-  })
-
-  app.post('/updatestudent', async (req, res) => {
-    dataCollection.findOneAndUpdate(
-      {
-      student_id: parseInt(req.body.student_id)
-    },
-      {
-        $set:
+    //POST that works as a PUT to update users entered in the Update user form. Uses student_id as an identifier
+    app.post('/updatestudent', async (req, res) => {
+      dataCollection.findOneAndUpdate(
         {
-      name: req.body.name,
-      surname: req.body.surname,
-      age: req.body.age,
-      nationality: req.body.nationality,
-      degree: req.body.degree,
-      dateAdded: req.body.dateAdded
-    }
-  },
-  ).then((result) => {
-    res.redirect('/')
-  })
-  .catch((error) => console.error(error));
-})
-});
+          student_id: parseInt(req.body.student_id)
+        },
+        {
+          $set:
+          {
+            name: req.body.name,
+            surname: req.body.surname,
+            age: req.body.age,
+            nationality: req.body.nationality,
+            degree: req.body.degree,
+            dateAdded: req.body.dateAdded
+          }
+        },
+      ).then((result) => {
+        res.redirect('/')
+      })
+        .catch((error) => console.error(error));
+    })
+  });
--- userSchema.js
+++ userSchema.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
+//Schema for making new users
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    surname: {type:String, required:true},
-    student_id: {type:Number, required:true},
-    age: {type:Number, required:true},
-    nationality: {type:String, required:true},
-    degree: {type:String, required:true},
-    dateAdded: {type: Date, required: true, default: Date.now}
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    student_id: { type: Number, required: true },
+    age: { type: Number, required: true },
+    nationality: { type: String, required: true },
+    degree: { type: String, required: true },
+    dateAdded: { type: Date, required: true, default: Date.now }
 })
+
 module.exports = mongoose.model("Users", userSchema, "user_data")
